4b8dbcdf5764c8360624e6495869fbcf55f6d709
github username <github email address>
2019-06-17 16:25:25 -0400

    added deck_id to cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_202506) do
+ActiveRecord::Schema.define(version: 2019_06_17_202059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_06_16_202506) do
     t.string "gameFormat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "deck_id"
   end
 
   create_table "decks", force: :cascade do |t|
