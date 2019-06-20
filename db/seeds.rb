6aa43c4dbfe536c07b3731bc0a8d9d50f6f21d66
github username <github email address>
2019-06-20 09:43:32 -0400

    friday morning

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.new(username: 'tarvis', password: 'tarvis', avatar_img: 'https://cdn.drawception.com/images/panels/2017/6-24/dWqG1O57LE-8.png').save
 Deck.new(user_id: 1, name: 'Bant Nexus Control', color: '{U}{G}{W}', description: "Strategy: don't let your opponents play the game").save
 Card.new(
