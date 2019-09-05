41f657bc2263098557744346086a9609dac34468
github username <travisemerymartin@gmail.com>
2019-09-05 15:07:41 -0400

    fixed spelling error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :decks, only: [:index, :show, :create, :update, :destroy]
-  resources :cards, only: [:index, :show, :create, :update, :destory]
+  resources :cards, only: [:index, :show, :create, :update, :destroy]
   post '/login', to: 'sessions#create_session', as: 'login'
   post '/search', to: 'searches#cards', as: 'search'
   post '/submitDeck', to: 'cards#intakeList', as: 'intakeList'
