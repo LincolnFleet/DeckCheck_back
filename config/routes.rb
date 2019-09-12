f14e63e10ac050a04d4aab4bb82da5f59a57f241
github username <travisemerymartin@gmail.com>
2019-09-12 13:55:46 -0400

    Added :destroy dependencies for User and Deck

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
-  resources :users, only: [:index, :show, :create, :update, :destroy]
-  resources :decks, only: [:index, :show, :create, :update, :destroy]
-  resources :cards, only: [:index, :show, :create, :update, :destroy]
+  delete '/decks', to: 'decks#destroy', as: 'delete'
+  resources :users, only: [:index, :show, :create, :update, :delete]
+  resources :decks, only: [:index, :show, :create, :update, :delete]
+  resources :cards, only: [:index, :show, :create, :update, :delete]
   post '/login', to: 'sessions#create_session', as: 'login'
   post '/search', to: 'searches#cards', as: 'search'
   post '/submitDeck', to: 'cards#intakeList', as: 'intakeList'
